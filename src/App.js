0107a213cea8597384ee9e53c7b279490bd00e17
Mariely Delacruz <mdlc0502@gmail.com>
2017-09-21 23:45:05 -0400

    added NoteForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.addNote = this.addNote.bind(this);
 
     // Setup the React state of our component
     this.state = {
@@ -14,6 +16,15 @@ class App extends Component {
         { id: 2, noteContent: "Note 2 here"},
       ],
     }
+  }
+
+  addNote(note){
+    //push the note onto the notes array
+    const previousNotes = this.state.notes;
+    previousNotes.push({ id:previousNotes.length + 1, noteContent: note });
+    this.setState({
+      notes: previousNotes
+    })
   }
   render() {
     return (
@@ -28,10 +39,11 @@ class App extends Component {
             <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
           )
         })
-        
+
       }
       </div>
       <div className="notesFooter">
+      <NoteForm addNote={this.addNote} />
       </div>
       </div>
     );
